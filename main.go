0ffa25db01673ee3734e23b9abe91113c968d2c7
Jamie Alquiza <ja@grey-boundary.io>
2015-04-06 10:02:43 -0600

    optional timestamp and hostname fields

--- main.go
+++ main.go
@@ -1,3 +1,24 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Jamie Alquiza
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 package main
 
 import (
@@ -12,7 +33,7 @@ import (
 
 var (
 	hostname     string
-	ts string
+	ts           string
 	tagHostname  bool
 	tagTimestamp bool
 )
@@ -29,8 +50,8 @@ type connection struct {
 	RemoteIp   string `json:"remote_ip"`
 	RemotePort string `json:"remote_port"`
 	State      string `json:"state"`
-	Timestamp  string `json:"@timestamp"`
-	Hostname   string `json:"@hostname"`
+	Timestamp  string `json:"@timestamp,omitempty"`
+	Hostname   string `json:"@hostname,omitempty"`
 }
 
 func main() {
@@ -45,7 +66,8 @@ func main() {
 	conns, _ := tcpconns.Get()
 	for _, c := range conns {
 
-		m := &connection{LocalIp: c[0],
+		m := &connection{
+			LocalIp:    c[0],
 			LocalPort:  c[1],
 			RemoteIp:   c[2],
 			RemotePort: c[3],
